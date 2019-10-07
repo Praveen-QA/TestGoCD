5e20524b259116a65c287d57f5ec3ec11db80fae
Praveen-QA <54061798+Praveen-QA@users.noreply.github.com>
2019-10-07 17:30:18 +0530

    fixed

--- SampleWebApp/src/test/java/com/mypackage/it/SampleProjectApplicationTests.java
+++ SampleWebApp/src/test/java/com/mypackage/it/SampleProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.mypackage.it;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
